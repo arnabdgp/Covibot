d5c0c31b0e678fb75b3c9810ef2c311c0d4d57c3
arnabdgp <arnab.sinhamahapatra@yahoo.in>
2021-10-12 14:56:38 +0530

    First python app

--- run.py
+++ run.py
@@ -1,14 +1,12 @@
+#  https://basicdashflask.herokuapp.com/
 import flask
 from dashboard.dash1 import create_dash1_application
-from dashboard.dash2 import create_dash2_application
 
 server = flask.Flask(__name__)
 
 create_dash1_application(server)
-create_dash2_application(server)
 
 @server.route('/')
-@server.route('/hello')
 def hello():
     return flask.redirect('dash1')
 
