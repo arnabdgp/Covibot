7612e0276db4515681e2b14b015fbcb4189d9d76
arnabdgp <arnab.sinhamahapatra@yahoo.in>
2021-12-29 11:32:26 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #  https://basicdashflask.herokuapp.com/
 from flask import *
-import torch
-from transformers import BertTokenizer, BertModel
+# import torch
+# from transformers import BertTokenizer, BertModel
 import matplotlib.pyplot as plt
 # Load pre-trained model tokenizer (vocabulary)
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 import pandas as pd
 import numpy as np
 import re
@@ -14,81 +14,81 @@ import json
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
-import nltk
-nltk.download('punkt')
-nltk.download('brown')
+# import nltk
+# nltk.download('punkt')
+# nltk.download('brown')
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob
 import numpy as np
-from scipy.spatial.distance import cosine
-from sentence_transformers import SentenceTransformer
+# from scipy.spatial.distance import cosine
+# from sentence_transformers import SentenceTransformer
 from dashboard.sentiment import create_sentiment_application
 
 server = Flask(__name__, template_folder='static')
 
 create_sentiment_application(server)
 
-df=pd.read_csv('covid19_tweets.csv')
-df=df.rename(columns={'text':'tweet'})
-documents=pd.DataFrame(df['tweet'][0:100])
-def pre_process(tweet):
-    tweet = re.sub(r'\$\w*', '', tweet)
-    tweet = re.sub(r'^RT[\s]+', '', tweet)
-    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
-    tweet = re.sub(r'#', '', tweet )
-    tweet = re.sub(r'\n', ' ', tweet )
+# df=pd.read_csv('covid19_tweets.csv')
+# df=df.rename(columns={'text':'tweet'})
+# documents=pd.DataFrame(df['tweet'][0:100])
+# def pre_process(tweet):
+#     tweet = re.sub(r'\$\w*', '', tweet)
+#     tweet = re.sub(r'^RT[\s]+', '', tweet)
+#     tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
+#     tweet = re.sub(r'#', '', tweet )
+#     tweet = re.sub(r'\n', ' ', tweet )
     #tweet = re.sub(r'@', '', tweet )
-    tweet=' '.join(word for word in tweet.split(' ') if not word.startswith('@'))        
-    return tweet
+#     tweet=' '.join(word for word in tweet.split(' ') if not word.startswith('@'))        
+#     return tweet
 
-processed_docs = documents['tweet'].map(pre_process)
-documents['processed_tweets']=processed_docs
+# processed_docs = documents['tweet'].map(pre_process)
+# documents['processed_tweets']=processed_docs
 
-def isNotBlank (myString):
-    if myString and myString.strip():
+# def isNotBlank (myString):
+    # if myString and myString.strip():
         #myString is not None AND myString is not empty or blank
-        return True
+        # return True
         #myString is None OR myString is empty or blank
-    return False
+    # return False
     
-model = BertModel.from_pretrained('bert-base-uncased',
-                                 output_hidden_states = True, # Whether the model returns all hidden-states.
-                                 )
+# model = BertModel.from_pretrained('bert-base-uncased',
+#                                  output_hidden_states = True, # Whether the model returns all hidden-states.
+#                                  )
 
     #Put the model in "evaluation" mode, meaning feed-forward operation.
-model.eval()
+# model.eval()
 
-data=pd.DataFrame()
-def get_embeddings(text):
-    marked_text = "[CLS] " + text + " [SEP]"
-    tokenized_text = tokenizer.tokenize(marked_text) 
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-    segments_ids = [1] * len(tokenized_text)
-    tokens_tensor = torch.tensor([indexed_tokens])
-    segments_tensors = torch.tensor([segments_ids])
-    with torch.no_grad():
-        outputs = model(tokens_tensor, segments_tensors)
-        hidden_states = outputs[2]
-    token_embeddings = torch.stack(hidden_states, dim=0)
-    token_embeddings.size()
-    token_embeddings = torch.squeeze(token_embeddings, dim=1)
-    token_embeddings.size()
-    token_embeddings = token_embeddings.permute(1,0,2)
-    token_embeddings.size()
-    token_vecs_sum = []
-    a=np.zeros(shape=(len(token_embeddings),768))
-    for i in range(0,len(token_embeddings)):
-        sum_vec = torch.sum(token_embeddings[i][-4:], dim=0)
-        token_vecs_sum.append(sum_vec)
-        a[i]=sum_vec
-    a=np.average(a,axis=0)
-    return a
+# data=pd.DataFrame()
+# def get_embeddings(text):
+#     marked_text = "[CLS] " + text + " [SEP]"
+#     tokenized_text = tokenizer.tokenize(marked_text) 
+#     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+#     segments_ids = [1] * len(tokenized_text)
+#     tokens_tensor = torch.tensor([indexed_tokens])
+#     segments_tensors = torch.tensor([segments_ids])
+#     with torch.no_grad():
+#         outputs = model(tokens_tensor, segments_tensors)
+#         hidden_states = outputs[2]
+#     token_embeddings = torch.stack(hidden_states, dim=0)
+#     token_embeddings.size()
+#     token_embeddings = torch.squeeze(token_embeddings, dim=1)
+#     token_embeddings.size()
+#     token_embeddings = token_embeddings.permute(1,0,2)
+#     token_embeddings.size()
+#     token_vecs_sum = []
+#     a=np.zeros(shape=(len(token_embeddings),768))
+#     for i in range(0,len(token_embeddings)):
+#         sum_vec = torch.sum(token_embeddings[i][-4:], dim=0)
+#         token_vecs_sum.append(sum_vec)
+#         a[i]=sum_vec
+#     a=np.average(a,axis=0)
+#     return a
 
-documents['embeddings']=documents['processed_tweets'].apply(lambda x: get_embeddings(x))
+# documents['embeddings']=documents['processed_tweets'].apply(lambda x: get_embeddings(x))
 # from scipy.spatial.distance import cosine
-def cosine_similarity(x,y):
-    similarity = cosine(x,y)
-    return similarity
+# def cosine_similarity(x,y):
+#     similarity = cosine(x,y)
+#     return similarity
     
 # query='covid active cases'
 # embedding=get_embeddings(query)
@@ -254,10 +254,10 @@ def covidnewssearch():
 # Convert the response to JSON format and pretty print it
 # response_json = response.json()
 
-@server.route('/tweets',methods=['POST'])
-def searchbar():
-    query_text = request.form['search_tweets']
-    query = query_text
+# @server.route('/tweets',methods=['POST'])
+# def searchbar():
+    # query_text = request.form['search_tweets']
+    # query = query_text
     # query_vec = sbert_model.encode([query])[0]
     # data=pd.DataFrame()
     # for sent in sentences:
@@ -265,7 +265,7 @@ def searchbar():
     #     data=data.append({'text':sent,'similarity':sim},ignore_index=True)
     
     # data=data.sort_values(by=['similarity'],ascending=False).reset_index()
-    return render_template('tweet_result.html',tweet1 = data['text'][0],tweet2 = data['text'][1],tweet3 = data['text'][2],tweet4 = data['text'][3],tweet5 = data['text'][4])
+    # return render_template('tweet_result.html',tweet1 = data['text'][0],tweet2 = data['text'][1],tweet3 = data['text'][2],tweet4 = data['text'][3],tweet5 = data['text'][4])
 
 
 if __name__ == '__main__':
